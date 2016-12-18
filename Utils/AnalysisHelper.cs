c022acf9cd20870a713a2ffef448a9accc0128f6
Hu <NMSLanX@users.noreply.github.com>
2016-12-18 13:36:05 +0800

    辅助类

---
+++ Utils/AnalysisHelper.cs
@@ -0,0 +1,148 @@
+﻿/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
+using NMSReflector.Cache;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+namespace NMSReflector.Utils
+{
+    internal sealed class AnalysisHelper
+    {
+        public AnalysisHelper()
+        {
+
+        }
+
+        public static void CreateCache(Type temp_Type)
+        {
+
+            ColumnAttribute temp_ColumnAttribute = null;
+            Dictionary<string, Func<object, object>> GetDict = new Dictionary<string, Func<object, object>>();
+            Dictionary<string, Action<object, object>> SetDict = new Dictionary<string, Action<object, object>>();
+            Dictionary<string, string> MapDict = new Dictionary<string, string>();
+            Dictionary<string, string> ReversionMapDict = new Dictionary<string, string>();
+            Dictionary<string, Type> TypeDict = new Dictionary<string, Type>();
+            Dictionary<string, MethodInfo> GetMethodInfoDict = new Dictionary<string, MethodInfo>();
+            Dictionary<string, MethodInfo> SetMethodInfoDict = new Dictionary<string, MethodInfo>();
+            Dictionary<string, FieldInfo> FieldInfoDict = new Dictionary<string, FieldInfo>();
+            //拿到Emit反射方法
+            WeakReference<EmitHelper> emitHandler = new WeakReference<EmitHelper>(new EmitHelper(temp_Type), false);
+            EmitHelper tempNode = null;
+            while (!emitHandler.TryGetTarget(out tempNode))
+            {
+                emitHandler.SetTarget(new EmitHelper(temp_Type));
+                emitHandler.TryGetTarget(out tempNode);
+            }
+
+            #region 属性Emit
+            PropertyInfo[] tPropertyList = temp_Type.GetProperties();
+
+            string FlagName = string.Empty;
+
+            int i_length = tPropertyList.Length;
+            int i = 0;
+            PropertyInfo temp_Property = null;
+            while (i<i_length)
+            {
+                temp_Property = tPropertyList[i];
+
+                FlagName = temp_Property.Name;
+                GetDict[FlagName] = tempNode.EmitGetMethod(temp_Property);
+                SetDict[FlagName] = tempNode.EmitSetMethod(temp_Property);
+                TypeDict[FlagName] = temp_Property.PropertyType;
+                ReversionMapDict[FlagName] = FlagName;
+                GetMethodInfoDict[FlagName] = temp_Property.GetGetMethod(true);
+                SetMethodInfoDict[FlagName] = temp_Property.GetSetMethod(true);
+                //添加映射
+                temp_ColumnAttribute = temp_Property.GetCustomAttribute<ColumnAttribute>();
+                if (temp_ColumnAttribute != null)
+                {
+                    FlagName = temp_ColumnAttribute.Name;
+                    //添加到映射字典
+                    ReversionMapDict[FlagName] = FlagName;
+
+                    //获取Get方法
+                    GetDict[FlagName] = GetDict[temp_Property.Name];
+
+                    //获取Set方法
+                    SetDict[FlagName] = SetDict[temp_Property.Name];
+
+                    //获取Type
+                    TypeDict[FlagName] = TypeDict[temp_Property.Name];
+
+                    GetMethodInfoDict[FlagName] = temp_Property.GetGetMethod(true);
+                    SetMethodInfoDict[FlagName] = temp_Property.GetSetMethod(true);
+                }
+                MapDict[FlagName] = temp_Property.Name;
+                i += 1;
+            }
+            #endregion
+
+            #region 字段Emit
+            FieldInfo[] tFieldList = temp_Type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            i = 0;
+            i_length = tFieldList.Length;
+            FieldInfo item_Field = null;
+            while (i<i_length)
+            {
+                item_Field = tFieldList[i];
+                FlagName = item_Field.Name;
+                GetDict[FlagName] = tempNode.EmitGetMethod(item_Field);
+                SetDict[FlagName] = tempNode.EmitSetMethod(item_Field);
+                TypeDict[FlagName] = item_Field.FieldType;
+                ReversionMapDict[FlagName] = FlagName;
+                FieldInfoDict[FlagName] = item_Field;
+                temp_ColumnAttribute = item_Field.GetCustomAttribute<ColumnAttribute>();
+                if (temp_ColumnAttribute != null)
+                {
+                    FlagName = temp_ColumnAttribute.Name;
+                    //添加到映射字典
+                   
+                    ReversionMapDict[item_Field.Name] = FlagName;
+
+                    //获取Get方法
+                    GetDict[temp_ColumnAttribute.Name] = GetDict[item_Field.Name];
+
+                    //获取Set方法
+                    SetDict[temp_ColumnAttribute.Name] = SetDict[item_Field.Name];
+
+                    //获取Type
+                    TypeDict[temp_ColumnAttribute.Name] = TypeDict[item_Field.Name];
+
+                    FieldInfoDict[FlagName] = item_Field;
+                }
+                MapDict[FlagName] = item_Field.Name;
+                
+                i += 1;
+            }
+            #endregion
+
+            TempCache.GetMethodCache[temp_Type] = GetDict;
+            TempCache.SetMethodCache[temp_Type] = SetDict;
+            TempCache.ModelPropertyMapCache[temp_Type] = MapDict;
+            TempCache.ModelTypeCache[temp_Type] = TypeDict;
+            TempCache.ModelPropertyReversionMapCache[temp_Type] = ReversionMapDict;
+            TempCache.SetMethodInfoCache[temp_Type] = SetMethodInfoDict;
+            TempCache.GetMethodInfoCache[temp_Type] = GetMethodInfoDict;
+            TempCache.FieldInfoCache[temp_Type] = FieldInfoDict;
+
+        }
+
+
+    }
+}
---
+++ Utils/EmitHelper.cs
@@ -0,0 +1,195 @@
+﻿/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace NMSReflector.Utils
+{
+    internal sealed class EmitHelper
+    {
+        public Type EmitType;
+        public EmitHelper(Type temp_Type)
+        {
+            EmitType = temp_Type;
+        }
+        /// <summary>
+        /// 根据类的属性建立Set方法的委托。
+        /// </summary>
+        /// <param name="property">类的属性</param>
+        /// <returns></returns>
+        public Action<object, object> EmitSetMethod(PropertyInfo property)
+        {
+            MethodInfo method = property.GetSetMethod(true);
+
+            if (method==null)
+            {
+                return null;
+            }
+
+            DynamicMethod newMethod = new DynamicMethod(property.Name + "_Setter",
+                null,
+                new Type[] { typeof(object), typeof(object) }
+                );
+            ILGenerator il = newMethod.GetILGenerator();
+
+            if (!method.IsStatic)
+            {
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Castclass, EmitType);
+            }
+
+            il.Emit(OpCodes.Ldarg_1);
+
+            if (property.PropertyType.IsValueType)
+            {
+                il.Emit(OpCodes.Unbox_Any, property.PropertyType);
+            }
+            else
+            {
+                il.Emit(OpCodes.Castclass, property.PropertyType);
+            }
+            if (!method.IsStatic)
+            {
+                il.EmitCall(OpCodes.Callvirt, method, null);
+            }
+            else
+            {
+                il.EmitCall(OpCodes.Call, method, null);
+            }
+            il.Emit(OpCodes.Ret);
+            return (Action<object, object>)newMethod.CreateDelegate(typeof(Action<object, object>));
+        }
+        /// <summary>
+        /// 根据类的属性建立Get方法的委托。
+        /// </summary>
+        /// <param name="property">类的属性</param>
+        /// <returns></returns>
+        public Func<object, object> EmitGetMethod(PropertyInfo property)
+        {
+            MethodInfo method = property.GetGetMethod(true);
+
+            if (method == null)
+            {
+                return null;
+            }
+            DynamicMethod newMethod = new DynamicMethod(property.Name + "_Getter",
+               typeof(object),
+               new Type[] { typeof(object) }
+               );
+
+            ILGenerator il = newMethod.GetILGenerator();
+            if (!method.IsStatic)
+            {
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Castclass, EmitType);
+                il.EmitCall(OpCodes.Callvirt, method, null);
+            }
+            else
+            {
+                il.EmitCall(OpCodes.Call, method, null);
+            }
+            if (property.PropertyType.IsValueType)
+            {
+                il.Emit(OpCodes.Box, property.PropertyType);
+            }
+
+            il.Emit(OpCodes.Ret);
+
+            return (Func<object, object>)newMethod.CreateDelegate(typeof(Func<object, object>));
+        }
+        /// <summary>
+        /// 根据类的字段建立Set方法的委托。
+        /// </summary>
+        /// <param name="field">类的字段</param>
+        /// <returns></returns>
+        public Action<object, object> EmitSetMethod(FieldInfo field)
+        {
+            //创建动态函数，无返回值，两个参数
+            DynamicMethod newMethod = new DynamicMethod(field.Name + "_Setter",
+                null,
+                new Type[] { typeof(object), typeof(object) }
+                );
+            ILGenerator il = newMethod.GetILGenerator();
+
+            //非静态属性，加载第一个参数
+            if (!field.IsStatic)
+            {
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Castclass, EmitType);
+            }
+            //加载第二个参数
+            il.Emit(OpCodes.Ldarg_1);
+
+            //从object拆箱
+            if (field.FieldType.IsValueType)
+            {
+                il.Emit(OpCodes.Unbox_Any, field.FieldType);
+            }
+            else
+            {
+                il.Emit(OpCodes.Castclass, field.FieldType);
+            }
+            if (!field.IsStatic)
+            {
+                il.Emit(OpCodes.Stfld, field);
+            }
+            else
+            {
+                il.Emit(OpCodes.Stsfld, field);
+            }
+            
+            il.Emit(OpCodes.Ret);
+            return (Action<object, object>)newMethod.CreateDelegate(typeof(Action<object, object>));
+        }
+        /// <summary>
+        /// 根据类的字段建立Get方法的委托。
+        /// </summary>
+        /// <param name="field">类的字段</param>
+        /// <returns></returns>
+        public Func<object, object> EmitGetMethod(FieldInfo field)
+        {
+            //创建动态方法，一个返回参数，一个传入参数
+            DynamicMethod newMethod = new DynamicMethod(field.Name + "_Getter",
+               typeof(object),
+               new Type[] { typeof(object) }
+               );
+
+            ILGenerator il = newMethod.GetILGenerator();
+            if (!field.IsStatic)
+            {
+                //非静态属性Get获取器
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Castclass, EmitType);
+                il.Emit(OpCodes.Ldfld, field);
+            }
+            else
+            {
+                il.Emit(OpCodes.Ldsfld, field);
+            }
+            
+            if (field.FieldType.IsValueType)
+            {
+                //值类型装箱
+                il.Emit(OpCodes.Box, field.FieldType);
+            }
+
+            il.Emit(OpCodes.Ret);
+
+            return (Func<object, object>)newMethod.CreateDelegate(typeof(Func<object, object>));
+        }
+    }
+}
